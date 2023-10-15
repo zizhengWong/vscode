8613de04751011ec8edd1341ebf91a5e8ba6501b
zizheng02 <zizheng.wang02@gmail.com>
2023-10-15 23:15:40 +0800

    v1

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "header/stdc++.h"
+// #include<iostream>
+// #include<string>
+// #include<vector>
+using namespace std;
+
+int main(){
+    int b = 1;
+    // auto a = b;
+    // cout << a << endl;
+    // vector<string> msg{"Hello", "C++", "World", "from", "VS Code"};
+    // int len = msg.size();
+    // cout << "长度" << len << endl;
+    for (int i=0; i<4; i++){
+        // ms.push_back("a");
+        cout << i << endl;
+    }
+
+    return 0;
+}
